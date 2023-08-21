5870562dedad66f379526aeec928a5ed6dbee6f5
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-08-21 16:13:09 -0400

    added icon to page tab and loaded test content of renderer

--- final-arc/src/App.jsx
+++ final-arc/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className={" bg-indigo-800 absolute w-full h-screen p-0 top-0 left-0"}>
+			<div className={"h-5/6 p-0 "}>
+				<Canvas
+					camera={{
+						position: [0, 0, 7],
+						fov: 30,
+					}}
+				>
+					<color
+						attach='background'
+						args={["#ececec"]}
+					/>
+
+					<OrbitControls />
+					<mesh rotation={[Math.PI / 10, 10, 10]}>
+						<torusGeometry />
+						<meshNormalMaterial />
+					</mesh>
+				</Canvas>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
