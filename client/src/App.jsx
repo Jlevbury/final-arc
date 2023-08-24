47c2b1d70a56187dffb873b7ddeaee9004400747
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-08-24 17:37:13 -0400

    preclass push of current state, adding body sections to start developing card content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import { useState, useMemo } from "react";
 import "./App.css";
 
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Stats, Center } from "@react-three/drei";
 import { CameraTargetProvider, Navbar, MainCanvas } from "./components";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Navbar />
 			<CameraTargetProvider>
-				<MainCanvas />
+				<div className='relative z-0 bg-primary'>
+					<div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+						<Navbar />
+
+						<MainCanvas />
+					</div>
+				</div>
 			</CameraTargetProvider>
 		</BrowserRouter>
 	);
--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,7 +1,8 @@
-export default function VideoText(props) {
+import { VideoTextureProps } from "@react-three/fiber";
+export default function Video(props) {
 	const [video] = useState(() =>
 		Object.assign(document.createElement("video"), {
-			src: "/drei.mp4",
+			src: "../assets/video/bokehnav.mp4",
 			crossOrigin: "Anonymous",
 			loop: true,
 			muted: true,
@@ -9,20 +10,44 @@ export default function VideoText(props) {
 	);
 	useEffect(() => void video.play(), [video]);
 	return (
-		<Text
-			font='/Inter-Bold.woff'
-			fontSize={3}
-			letterSpacing={-0.06}
-			{...props}
-		>
-			drei
-			<meshBasicMaterial toneMapped={false}>
-				<videoTexture
-					attach='map'
-					args={[video]}
-					encoding={THREE.sRGBEncoding}
-				/>
-			</meshBasicMaterial>
-		</Text>
+		<meshBasicMaterial toneMapped={false}>
+			<videoTexture
+				attach='map'
+				args={[video]}
+				encoding={THREE.sRGBEncoding}
+			/>
+		</meshBasicMaterial>
+	);
+}
+
+function Scene() {
+	const size = useAspect(1800, 1000);
+	return (
+		<mesh scale={size}>
+			<planeGeometry />
+			<Suspense fallback={<FallbackMaterial url='10.jpg' />}>
+				<VideoMaterial url='10.mp4' />
+			</Suspense>
+		</mesh>
+	);
+}
+
+function VideoMaterial({ url }) {
+	const texture = useVideoTexture(url);
+	return (
+		<meshBasicMaterial
+			map={texture}
+			toneMapped={false}
+		/>
+	);
+}
+
+function FallbackMaterial({ url }) {
+	const texture = useTexture(url);
+	return (
+		<meshBasicMaterial
+			map={texture}
+			toneMapped={false}
+		/>
 	);
 }
