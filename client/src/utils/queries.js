f058582abb21243b5d1594e5cb86918ba4285d87
Austin W <auswaller@gmail.com>
2023-08-27 18:02:43 -0400

    added queries for getting the current user and a user's game list

---
+++ client/src/utils/queries.js
@@ -0,0 +1,34 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      games {
+        _id
+        rawgId
+        name
+        image
+        rating
+      }
+    }
+  }
+`;
+
+export const QUERY_GAMES = gql`
+  query getGames($username: String) {
+    user(username: $username) {
+      _id
+      username
+      email
+      games {
+        _id
+        rawgId
+        name
+        image
+        rating
+    }
+  }
+`;
