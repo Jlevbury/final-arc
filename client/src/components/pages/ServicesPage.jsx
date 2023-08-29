906345f481298663592a76344a7bff191231b917
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-08-28 22:27:16 -0400

    push before bed!

--- client/src/components/pages/ServicesPage.jsx
+++ client/src/components/pages/ServicesPage.jsx
@@ -9,51 +9,44 @@ export default function ServicesPage() {
 	pageTitle("Final Arc ");
 
 	useEffect(() => {
-		// Using destructured import
 		window.scrollTo(0, 0);
 	}, []);
 
 	return (
 		<>
 			<Div className='container'>
+				<Hero />
+
+				<Spacing
+					lg='15'
+					md='15'
+				/>
+
 				<Div className='row'>
-					<Div className='col-xl-4'></Div>
-					<Hero />
-					{/* Spacing appears to be inconsistent; added more uniform spacing */}
-					<Spacing
-						lg='15'
-						md='15'
-					/>
-					<Div className='col-xl-8'>
-						<Div className='row'>
-							<Div className='col-lg-3 col-sm-6 cs-hidden_mobile'></Div>
-							<Div className='col-lg-3 col-sm-6'>
-								<Card
-									title='Emu.Js online emulation'
-									link='/emulator'
-									src='../../../public/image/SVG/emuLink.svg'
-									alt='Emu JS'
-								/>
-								<Spacing
-									lg='15'
-									md='15'
-								/>
-							</Div>
-							<Div className='col-lg-3 col-sm-6 cs-hidden_mobile'></Div>
-							<Div className='col-lg-3 col-sm-6'>
-								<Card
-									title='RAWG Gaming API'
-									link='/gamecollection'
-									src='../../../public/image/SVG/rawgLink.svg'
-									alt='RAWG API'
-								/>
-								<Spacing
-									lg='15'
-									md='30'
-								/>{" "}
-								{/* Spacing is inconsistent; consider making it uniform */}
-							</Div>
-						</Div>
+					<Div className='col-lg-3 col-sm-6  cs-shine_hover_1 '>
+						<Card
+							title=''
+							link='/emulator'
+							src='../../../public/image/SVG/emuLink.svg'
+							alt='Emu JS'
+						/>
+						<Spacing
+							lg='30'
+							md='60'
+						/>
+					</Div>
+
+					<Div className='col-lg-3 col-sm-6 cs-shine_hover_1 '>
+						<Card
+							title=''
+							link='/gamecollection'
+							src='../../../public/image/SVG/rawgLink.svg'
+							alt='RAWG API'
+						/>
+						<Spacing
+							lg='30'
+							md='60'
+						/>
 					</Div>
 				</Div>
 			</Div>
