8bd1781df3d3612a6bc4ace43c7feb7875127e23
Austin W <auswaller@gmail.com>
2023-08-30 07:50:22 -0400

    added apolloprovider to app so calls can be made with graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,22 +17,51 @@ import 'slick-carousel/slick/slick-theme.css';
 
 import FaqPage from './components/pages/FAQ';
 
+import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <CustomCursor />
-      <Routes>
-        <Route path='/' element={<Layout />} />
-        <Route path='/servicespage' element={<ServicesPage />} />
-        <Route path='/gamecollection' element={<GameCollection />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/teampage' element={<TeamPage />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/FaqPage' element={<FaqPage />} />
-        <Route path='/emulator' element={<Emulator />} />
-        <Route path='/collectionfinder' element={<CollectionFinder />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <CustomCursor />
+        <Routes>
+          <Route path='/' element={<Layout />} />
+          <Route path='/servicespage' element={<ServicesPage />} />
+          <Route path='/gamecollection' element={<GameCollection />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/teampage' element={<TeamPage />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/FaqPage' element={<FaqPage />} />
+          <Route path='/emulator' element={<Emulator />} />
+          <Route path='/collectionfinder' element={<CollectionFinder />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
